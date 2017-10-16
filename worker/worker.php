15d88026298f3191c7f699aeef120662e8f7257c
akihiro iwata <akihiro.iwata.1990@gmail.com>
2017-10-16 23:05:25 +0900

    add comment

--- worker/worker.php
+++ worker/worker.php
@@ -1,4 +1,5 @@
 <?php
+// Agent側で動くデーモンプロセスのサンプル
 require __DIR__ . '/../vendor/autoload.php';
 use PhpAmqpLib\Connection\AMQPStreamConnection;
 
@@ -10,17 +11,31 @@ if (PHP_SAPI == 'cli') {
         'guest' // RabbitMQパスワード
     );
     $channel = $connection->channel();
+
+    /*
+     * 先にworkerプロセスが立ち上がった場合、channelにqueueが登録されていない。
+     * この状態でconsumeを走らせるとエラーになるため、これを回避する。
+     * */
     $channel->queue_declare('queue', false, true, false, false);
 
     echo ' [*] Waiting for messages. To exit press CTRL+C', "\n";
 
     $callback = function ($msg) {
         echo " [x] Received ", $msg->body, "\n";
-        sleep(substr_count($msg->body, '.'));
+        instancesAdd(); // 仮想マシン起動
         echo " [x] Done", "\n";
+        /*
+         * acknowledgeを飛ばすことで、queueのタスクが完了したことをRabbitMQへ伝える。
+         * ackが飛んでこない状態でconnectionが切れた場合、RabbitMQは別のworkerへqueueを投げる。
+         * */
         $msg->delivery_info['channel']->basic_ack($msg->delivery_info['delivery_tag']);
     };
     $channel->basic_consume('queue', '', false, false, false, false, $callback);
+
+    /*
+     * RabbitMQが投げたQueueの数 - 返ってきたackの数が常に1以下になるようにする。
+     * 負荷分散のため。
+     * */
     $channel->basic_qos(null, 1, null);
 
     while (count($channel->callbacks)) {
@@ -31,6 +46,9 @@ if (PHP_SAPI == 'cli') {
     $connection->close();
 }
 
+/*
+ * 仮想マシンを起動する。
+ * */
 function instancesAdd()
 {
     // TODO: Call KVM Command
